5586cfc8432b792e52ede2de0527be0962bde763
Jeff Barfield <barfieldjt@gmail.com>
2023-05-28 18:46:38 -0400

    added images to categories

--- components/organisms/categories/CategoryListOrganism.tsx
+++ components/organisms/categories/CategoryListOrganism.tsx
@@ -1,67 +1,70 @@
 import { useRouter } from "next/router";
 
-    import { Card, Grid } from "@mui/material";
-    
-    import Breadcrumbs from "../../_molecules/Breadcrumbs";
-    
-    import { returnCurrentModule } from "../../../utils/helperFunctions";
-    import CategoryListCard from "../../_molecules/categories/CategoryListCard";
-    import CategoryCreationOrganism from "./CategoryCreationOrganism";
-    
-    import useCategories from "../../../hooks/categories/useCategories";
-    
-    const CategoryListOrganism = () => {
-        const router = useRouter();
-    
-        const categoriesData = useCategories();
-    
-        console.log(categoriesData);
-    
-        if (categoriesData.isLoading) {
-            return <div>Loading...</div>;
-        }
-    
-        const getHeroImage = (category: any) => {
-            return "https://images.unsplash.com/photo-1675127077743-f388e7e37924?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
-        };
-    
-        return (
-            <div>
-                <Breadcrumbs
-                    breadcrumbs={[
-                        {
-                            label: returnCurrentModule(router),
-                            anchor: `/admin/${returnCurrentModule(router)}`,
-                        },
-                        {
-                            label: `Categories`,
-                            anchor: null,
-                        },
-                    ]}
-                    actions={[
-                        {
-                            label: "Add New Category",
-                            component: <CategoryCreationOrganism />,
-                        },
-                    ]}
-                />
-                <Card sx={{ p: 2, my: 2 }}>
-                    <Grid container spacing={3}>
-                        {categoriesData?.data?.data?.map((category: any) => (
-                            <Grid item xs={12} sm={6} md={4} lg={3}>
-                                <CategoryListCard
-                                    title={category.category_name}
-                                    link={`/admin/${returnCurrentModule(router)}/categories/${
-                                        category.category_id
-                                    }`}
-                                    image={getHeroImage(category)}
-                                />
-                            </Grid>
-                        ))}
-                    </Grid>
-                </Card>
-            </div>
-        );
-    };
-    
-    export default CategoryListOrganism;
+import { Card, Grid } from "@mui/material";
+
+import Breadcrumbs from "../../_molecules/Breadcrumbs";
+
+import { returnCurrentModule } from "../../../utils/helperFunctions";
+import CategoryListCard from "../../_molecules/categories/CategoryListCard";
+import CategoryCreationOrganism from "./CategoryCreationOrganism";
+
+import useCategories from "../../../hooks/categories/useCategories";
+
+const CategoryListOrganism = () => {
+	const router = useRouter();
+
+	const categoriesData = useCategories();
+
+	console.log(categoriesData);
+
+	if (categoriesData.isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	const getHeroImage = (category: any) => {
+		if (category?.category_image) {
+			return category.category_image;
+		}
+		return "https://images.unsplash.com/photo-1675127077743-f388e7e37924?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80";
+	};
+
+	return (
+		<div>
+			<Breadcrumbs
+				breadcrumbs={[
+					{
+						label: returnCurrentModule(router),
+						anchor: `/admin/${returnCurrentModule(router)}`,
+					},
+					{
+						label: `Categories`,
+						anchor: null,
+					},
+				]}
+				actions={[
+					{
+						label: "Add New Category",
+						component: <CategoryCreationOrganism />,
+					},
+				]}
+			/>
+			<Card sx={{ p: 2, my: 2 }}>
+				<Grid container spacing={3}>
+					{categoriesData?.data?.data?.map((category: any) => (
+						<Grid item xs={12} sm={6} md={4} lg={3}>
+							<CategoryListCard
+								title={category.category_name}
+								link={`/admin/${returnCurrentModule(router)}/categories/${
+									category.category_id
+								}`}
+								image={getHeroImage(category)}
+							/>
+						</Grid>
+					))}
+				</Grid>
+			</Card>
+		</div>
+	);
+};
+
+export default CategoryListOrganism;
--- components/organisms/categories/CategoryOrganism.tsx
+++ components/organisms/categories/CategoryOrganism.tsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
+import Image from "next/image";
 import { useRouter } from "next/router";
 
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
@@ -13,6 +15,7 @@ import Breadcrumbs from "../../_molecules/Breadcrumbs";
 
 import CategoryDeletionOrganism from "./CategoryDeletionOrganism";
 
+import UploadButton from "../../_atoms/UploadButton";
 import AlertSnackbar from "../../_atoms/AlertSnackbar";
 
 import useCategory from "../../../hooks/categories/useCategory";
@@ -76,6 +79,7 @@ const CategoryOrganism = () => {
 	const category = useCategory(id);
 
 	const [updatedCategoryName, setUpdatedCategoryName] = useState<string | null>(null);
+	const [updatedCategoryImage, setUpdatedCategoryImage] = useState<string | null>(null);
 	// const [updatedCategoryCmsData, setUpdatedCategoryCmsData] = useState<[] | null>(null);
 
 	const [isCategorySaving, setIsCategorySaving] = useState(false);
@@ -86,6 +90,7 @@ const CategoryOrganism = () => {
 
 	const [updatedCategory, setUpdatedCategory] = useState<{
 		category_name: string | null;
+		category_image: string | null;
 		// cms_data: [] | null;
 	} | null>(null);
 
@@ -100,16 +105,19 @@ const CategoryOrganism = () => {
 
 	useEffect(() => {
 		setUpdatedCategoryName(category?.data?.data?.category_name);
+		setUpdatedCategoryImage(category?.data?.data?.category_image);
 		// setUpdatedCategoryCmsData(category?.data?.data?.cms_data);
 	}, [category.data]);
 
 	useEffect(() => {
 		setUpdatedCategory({
 			category_name: updatedCategoryName,
+			category_image: updatedCategoryImage,
 			// cms_data: updatedCategoryCmsData,
 		});
 	}, [
 		updatedCategoryName,
+		updatedCategoryImage,
 		// updatedCategoryCmsData,
 	]);
 
@@ -166,6 +174,44 @@ const CategoryOrganism = () => {
 						}}
 					>
 						<Grid container>
+							<Grid item xs={12}>
+								<Box
+									sx={{
+										position: "relative",
+										width: "100%",
+										height: "250px",
+										mb: 3,
+									}}
+								>
+									<Image
+										src={
+											updatedCategoryImage ||
+											"https://images.unsplash.com/photo-1596887245124-5150ad2491e7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80"
+										}
+										fill={true}
+										alt="test"
+										style={{ objectFit: "cover", borderRadius: "25px" }}
+									/>
+								</Box>
+								<TextField
+									sx={{ width: "100%", mb: 2 }}
+									multiline
+									id="outlined-name"
+									label="SEO Image"
+									value={updatedCategoryImage}
+									onChange={(e) => {
+										setUpdatedCategoryImage(e.target.value);
+										setHasContentBeenEdited(true);
+									}}
+									variant="standard"
+								/>
+							</Grid>
+							<Grid item xs={6}>
+								<UploadButton
+									setUpdatedHeroImage={setUpdatedCategoryImage}
+									setHasContentBeenEdited={setHasContentBeenEdited}
+								/>
+							</Grid>
 							<TextField
 								sx={{ width: "100%", mb: 2 }}
 								multiline
